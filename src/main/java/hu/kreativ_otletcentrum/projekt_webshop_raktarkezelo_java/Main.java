a569ff8b5362471aadfce678e41b49254c45daf9
ngabor0508 <ngabor0508@gmail.com>
2022-04-05 17:56:58 +0200

    Main osztály létrehozva.

---
+++ src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/Main.java
@@ -0,0 +1,7 @@
+package hu.kreativ_otletcentrum.projekt_webshop_raktarkezelo_java;
+
+public class Main {
+    public static void main(String[] args) {
+        RaktarKezeloApp.main(args);
+    }
+}
--- src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/RaktarKezeloApp.java
+++ src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/RaktarKezeloApp.java
@@ -12,7 +12,7 @@ public class RaktarKezeloApp extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(RaktarKezeloApp.class.getResource("main-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Kreatív Ötletcentrum Raktárkezelő!");
         stage.setScene(scene);
         stage.show();
     }
