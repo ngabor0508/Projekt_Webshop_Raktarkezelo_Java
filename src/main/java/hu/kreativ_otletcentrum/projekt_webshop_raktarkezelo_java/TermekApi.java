565a560073ae2148f5b38bd29761d57d3e8c473e
ngabor0508 <ngabor0508@gmail.com>
2022-04-06 13:43:04 +0200

    TermekApi osztályban getTermékek metódus elkezdve, gson konvertáló beimportálva.

--- src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/TermekApi.java
+++ src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/TermekApi.java
@@ -1,6 +1,15 @@
 package hu.kreativ_otletcentrum.projekt_webshop_raktarkezelo_java;
 
+import java.io.IOException;
+import java.util.List;
+
 public class TermekApi {
     private static final String BASE_URL = "http://localhost:8000";
     public static final String TERMEK_API_URL = BASE_URL + "/api/termekek";
+
+    public static List<Termek> getTermekek() throws IOException {
+        Response response = RequestHandler.get(TERMEK_API_URL);
+        String json = response.getContent();
+
+    }
 }
