c7017c6d9d3c78bd7e9749f6ad9e0ef76dc9b5f1
ngabor0508 <ngabor0508@gmail.com>
2022-04-06 12:54:30 +0200

    ApiError osztályban privát adattagra konstruktor generálása.

--- src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/ApiError.java
+++ src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/ApiError.java
@@ -3,4 +3,7 @@ package hu.kreativ_otletcentrum.projekt_webshop_raktarkezelo_java;
 public class ApiError {
     private String message;
 
+    public ApiError(String message) {
+        this.message = message;
+    }
 }
