6dcfd5a17e0fdfa53bdbb6da25cf00d797399b31
ngabor0508 <ngabor0508@gmail.com>
2022-04-06 13:06:23 +0200

    Response osztály kész, TermekApi osztályban privát, publikus adattagok(URL-ek) létrehozása.

--- src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/TermekApi.java
+++ src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/TermekApi.java
@@ -1,4 +1,8 @@
 package hu.kreativ_otletcentrum.projekt_webshop_raktarkezelo_java;
 
 public class TermekApi {
+    private static final String BASE_URL = "http://localhost:8000";
+    public static final String TERMEK_API_URL = BASE_URL + "/api/termekek";
+
+
 }
