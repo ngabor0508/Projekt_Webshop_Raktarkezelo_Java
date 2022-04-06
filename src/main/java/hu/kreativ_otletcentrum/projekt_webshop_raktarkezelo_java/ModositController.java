8b961c4552f62a9098e0943e0f755bbc079e5782
ngabor0508 <ngabor0508@gmail.com>
2022-04-06 12:35:39 +0200

    Modosit-view(Módosítás) kinézet kész.

--- src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/ModositController.java
+++ src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/ModositController.java
@@ -1,4 +1,29 @@
 package hu.kreativ_otletcentrum.projekt_webshop_raktarkezelo_java;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+
 public class ModositController {
+    @FXML
+    private TextField inputKodszam;
+    @FXML
+    private TextField inputName;
+    @FXML
+    private Spinner inputPrice;
+    @FXML
+    private Spinner inputQuantity;
+    @FXML
+    private TextField inputUrl;
+    @FXML
+    private ChoiceBox inputKategoria;
+
+
+
+
+    @FXML
+    public void onModositButtonClick(ActionEvent actionEvent) {
+    }
 }
