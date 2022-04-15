cdb4af1c040b5ff060c4aeaec58f70c4bae354a8
Nagy Gabor <ngabor0508@gmail.com>
2022-04-15 11:09:01 +0200

    Felhasznalo osztályban privát adattagok lérehozva, konstruktor generálása.

--- src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/Felhasznalo.java
+++ src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/Felhasznalo.java
@@ -1,4 +1,19 @@
 package hu.kreativ_otletcentrum.projekt_webshop_raktarkezelo_java;
 
+import java.util.Date;
+
 public class Felhasznalo {
+    private int id;
+    private String name;
+    private String email;
+    private Date created_at;
+
+    public Felhasznalo(int id, String name, String email, Date created_at) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.created_at = created_at;
+    }
+
+
 }
