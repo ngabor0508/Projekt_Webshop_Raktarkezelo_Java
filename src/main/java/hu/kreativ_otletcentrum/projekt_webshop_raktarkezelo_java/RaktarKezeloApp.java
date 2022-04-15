a16990ed26af7f97de8de327a6005d1066ec7d7b
Nagy Gabor <ngabor0508@gmail.com>
2022-04-15 13:57:42 +0200

    Bejelentkezés kinézet kész.

--- src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/RaktarKezeloApp.java
+++ src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/RaktarKezeloApp.java
@@ -10,9 +10,9 @@ import java.io.IOException;
 public class RaktarKezeloApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(RaktarKezeloApp.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Kreatív Ötletcentrum Raktárkezelő!");
+        FXMLLoader fxmlLoader = new FXMLLoader(RaktarKezeloApp.class.getResource("bejelentkezes-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 363, 258);
+        stage.setTitle("Kreatív Ötletcentrum Raktárkezelő Bejelentkezés!");
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/controllers/BejelentkezesController.java
+++ src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/controllers/BejelentkezesController.java
@@ -1,4 +1,17 @@
 package hu.kreativ_otletcentrum.projekt_webshop_raktarkezelo_java.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class BejelentkezesController {
+    @FXML
+    private TextField inputEmail;
+    @FXML
+    private PasswordField inputPassword;
+
+    @FXML
+    public void onBelepesButtonClick(ActionEvent actionEvent) {
+    }
 }
