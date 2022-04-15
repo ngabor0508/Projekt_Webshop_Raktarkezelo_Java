2d4e30b6d2a088cc1cd1ad85ed98dc1863d00701
Nagy Gabor <ngabor0508@gmail.com>
2022-04-15 11:05:46 +0200

    Token osztály kész. (konstruktor, getter, setter)

--- src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/Token.java
+++ src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/Token.java
@@ -1,4 +1,17 @@
 package hu.kreativ_otletcentrum.projekt_webshop_raktarkezelo_java;
 
 public class Token {
+    private String token;
+
+    public Token(String token) {
+        this.token = token;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
 }
