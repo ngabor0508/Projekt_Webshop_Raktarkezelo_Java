c17721ad65bcc20c02fd5bbbc466959bd4e563a9
Nagy Gabor <ngabor0508@gmail.com>
2022-04-15 11:04:10 +0200

    Login osztály kész. (privát adattagok, konstruktor)

--- src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/Login.java
+++ src/main/java/hu/kreativ_otletcentrum/projekt_webshop_raktarkezelo_java/Login.java
@@ -1,4 +1,11 @@
 package hu.kreativ_otletcentrum.projekt_webshop_raktarkezelo_java;
 
 public class Login {
+    private String email;
+    private String password;
+
+    public Login(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
 }
